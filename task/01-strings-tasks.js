0800368537d2e1be3ac0eac31c39e710969fe5f5
Anna <45462671+anna2990@users.noreply.github.com>
2019-10-15 10:31:05 +0200

    fixed

--- task/01-strings-tasks.js
+++ task/01-strings-tasks.js
@@ -243,11 +243,8 @@ function encodeToRot13(str) {
  *   isString(new String('test')) => true
  */
 function isString(value) {
-    if (typeof value === 'string' || value instanceof String){
-        return true;
-    } else {
-        return false;
-    }
+    return (typeof value === 'string' || value instanceof String) ? true : false;
+       
 }
 
 
