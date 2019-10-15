181e12e802803b26eb6361e905ef38eafda35614
Anna <45462671+anna2990@users.noreply.github.com>
2019-10-15 10:41:43 +0200

    fixed

--- task/03-date-tasks.js
+++ task/03-date-tasks.js
@@ -58,7 +58,7 @@ function parseDataFromIso8601(value) {
 function isLeapYear(date) {
    let d = new Date(date);
    let year = d.getFullYear();
-   return (year % 4 == 0) && !((year % 100 == 0) && !(year % 400 == 0));
+   return (year % 4 === 0) && !((year % 100 === 0) && !(year % 400 === 0));
 }
 
 
